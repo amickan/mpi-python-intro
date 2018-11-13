d3c8f8a1e51484a84156ef258a40645767df3b8d
amickan <a.mickan@donders.ru.nl>
2018-11-13 23:26:39 +0100

    finaly caught up on exercises from last wee s :-)

--- Session3/Session3.py
+++ Session3/Session3.py
@@ -3,3 +3,14 @@ def fibonnaci_fun(highest):
     while fibonnaci[-2]+fibonnaci[-1] < highest:
         fibonnaci.append(fibonnaci[-2]+fibonnaci[-1])
     return fibonnaci 
+
+def delduplicates_1(input_list):
+    output_list = []
+    for number in input_list:
+        if output_list.count(number) == False:
+            output_list.append(number)
+    output_list.sort()
+    return output_list
+    
+def delduplicates_2(input_list):
+    return list(set(input_list))
