412b1457c97421cda55e7df300cfdb08a4e1f004
amickan <a.mickan@donders.ru.nl>
2018-11-13 23:27:21 +0100

    Updating

--- Session3/helper_functions.py
+++ Session3/helper_functions.py
@@ -1,2 +1,19 @@
 def greet(name='stranger'):
     print(f'Hello, {name}!')
+    
+def fibonnaci_fun(highest):
+    fibonnaci = [0,1]
+    while fibonnaci[-2]+fibonnaci[-1] < highest:
+        fibonnaci.append(fibonnaci[-2]+fibonnaci[-1])
+    return fibonnaci 
+
+def delduplicates_1(input_list):
+    output_list = []
+    for number in input_list:
+        if output_list.count(number) == False:
+            output_list.append(number)
+    output_list.sort()
+    return output_list
+    
+def delduplicates_2(input_list):
+    return list(set(input_list))
